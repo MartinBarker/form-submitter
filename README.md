b3797555fa1b765294e051de940bbafa9eac336b
martinbarker <martinbarker99@gmail.com>
2022-12-01 12:33:28 -0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+Setup:
+1) clone/download this repo 
+2) cd into the repo folder 
+3) run `npm i` to install dependencies
+4) run `npm start` to begin
+
+if you don't want to see the web browser popup, change `headless: false` to `headless: true` on line `18` of `form-submitter.js`
+
---
+++ form-submitter.js
@@ -0,0 +1,66 @@
+const puppeteer = require('puppeteer')
+
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, time)
+  });
+}
+
+function generateRandomString(length, chars) {
+  var result = '';
+  for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+}
+
+console.log('begin')
+main();
+async function main() {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage()
+  await page.setViewport({ width: 1200, height: 800 })
+
+  console.log('start')
+
+  var numberOfSubmissions=0
+  while(true){
+    //goto url
+    await page.goto("https://www.defendkidstx.com/", { timeout: 0 });
+
+    //wait for button to load and click it
+    await page.waitForXPath("//span[text()='Report']")
+    const elements = await page.$x("//span[text()='Report']")
+    await elements[0].click() 
+
+    var randomCharsNoSpaces='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var randomCharsSpaces=' 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    
+    //enter name
+    await page.focus('input[data-original_id="name"]')
+    await page.keyboard.type(generateRandomString(16, randomCharsNoSpaces))
+
+    //enter email
+    await page.focus('input[data-original_id="email"]')
+    await page.keyboard.type(`${generateRandomString(8, randomCharsNoSpaces)}@${generateRandomString(8, randomCharsNoSpaces)}.com`)
+
+    //enter location of show
+    await page.focus('input[data-original_id="location_of_show"]')
+    await page.keyboard.type(generateRandomString(20, randomCharsSpaces))
+
+    //enter other info
+    await page.focus('textarea[data-original_id="other_info"]')
+    await page.keyboard.type(generateRandomString(69, randomCharsSpaces))
+
+    //click submit
+    await page.click('button[type="submit"]') 
+
+    //wait for success 
+    await page.waitForXPath("//p[text()='Your report has been sent, thank you!']")
+
+    numberOfSubmissions++
+    console.log(`submitted ${numberOfSubmissions} forms`)
+
+  }
+
+};
+
+
