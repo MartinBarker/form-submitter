ba8bf1295e307739f972d6f88729c8b9f47b2a6a
martinbarker <martinbarker99@gmail.com>
2022-12-01 12:38:22 -0800

    cleaner

--- form-submitter.js
+++ form-submitter.js
@@ -12,52 +12,58 @@ function generateRandomString(length, chars) {
   return result;
 }
 
-console.log('begin')
 main();
 async function main() {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage()
-  await page.setViewport({ width: 1200, height: 800 })
+  try{
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage()
+    await page.setViewport({ width: 1200, height: 800 })
+  }catch(err){
+    console.log('initial puppeteer setup err: ', err)
+  }
 
-  console.log('start')
 
   var numberOfSubmissions=0
   while(true){
-    //goto url
-    await page.goto("https://www.defendkidstx.com/", { timeout: 0 });
+      try{
+      //goto url
+      await page.goto("https://www.defendkidstx.com/", { timeout: 0 });
 
-    //wait for button to load and click it
-    await page.waitForXPath("//span[text()='Report']")
-    const elements = await page.$x("//span[text()='Report']")
-    await elements[0].click() 
+      //wait for button to load and click it
+      await page.waitForXPath("//span[text()='Report']")
+      const elements = await page.$x("//span[text()='Report']")
+      await elements[0].click() 
 
-    var randomCharsNoSpaces='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    var randomCharsSpaces=' 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    
-    //enter name
-    await page.focus('input[data-original_id="name"]')
-    await page.keyboard.type(generateRandomString(16, randomCharsNoSpaces))
+      var randomCharsNoSpaces='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      var randomCharsSpaces=' 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      
+      //enter name
+      await page.focus('input[data-original_id="name"]')
+      await page.keyboard.type(generateRandomString(16, randomCharsNoSpaces))
 
-    //enter email
-    await page.focus('input[data-original_id="email"]')
-    await page.keyboard.type(`${generateRandomString(8, randomCharsNoSpaces)}@${generateRandomString(8, randomCharsNoSpaces)}.com`)
+      //enter email
+      await page.focus('input[data-original_id="email"]')
+      await page.keyboard.type(`${generateRandomString(8, randomCharsNoSpaces)}@${generateRandomString(8, randomCharsNoSpaces)}.com`)
 
-    //enter location of show
-    await page.focus('input[data-original_id="location_of_show"]')
-    await page.keyboard.type(generateRandomString(20, randomCharsSpaces))
+      //enter location of show
+      await page.focus('input[data-original_id="location_of_show"]')
+      await page.keyboard.type(generateRandomString(20, randomCharsSpaces))
 
-    //enter other info
-    await page.focus('textarea[data-original_id="other_info"]')
-    await page.keyboard.type(generateRandomString(69, randomCharsSpaces))
+      //enter other info
+      await page.focus('textarea[data-original_id="other_info"]')
+      await page.keyboard.type(generateRandomString(69, randomCharsSpaces))
 
-    //click submit
-    await page.click('button[type="submit"]') 
+      //click submit
+      await page.click('button[type="submit"]') 
 
-    //wait for success 
-    await page.waitForXPath("//p[text()='Your report has been sent, thank you!']")
+      //wait for success 
+      await page.waitForXPath("//p[text()='Your report has been sent, thank you!']")
 
-    numberOfSubmissions++
-    console.log(`submitted ${numberOfSubmissions} forms`)
+      numberOfSubmissions++
+      console.log(`submitted ${numberOfSubmissions} forms`)
+    }catch(err){
+      console.log('program error:', err)
+    }
 
   }
 
