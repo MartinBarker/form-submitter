e1e8eab2fbde89445a2b6e0242969f1d0d1892ea
martinbarker99 <martinbarker99@gmail.com>
2022-12-01 12:49:15 -0800

    updated

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Setup:
 2) cd into the repo folder 
 3) run `npm i` to install dependencies
 4) run `npm start` to begin
+5) type `ctrl+c` into console to stop this program
 
 if you don't want to see the web browser popup, change `headless: false` to `headless: true` on line `18` of `form-submitter.js`
 
--- form-submitter.js
+++ form-submitter.js
@@ -14,29 +14,28 @@ function generateRandomString(length, chars) {
 
 main();
 async function main() {
-  try{
+
+  var numberOfSubmissions = 0
+
+  try {
+    //setup puppeteer
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage()
     await page.setViewport({ width: 1200, height: 800 })
-  }catch(err){
-    console.log('initial puppeteer setup err: ', err)
-  }
-
-
-  var numberOfSubmissions=0
-  while(true){
-      try{
+    
+    //begin loop
+    while(true){
       //goto url
       await page.goto("https://www.defendkidstx.com/", { timeout: 0 });
 
       //wait for button to load and click it
       await page.waitForXPath("//span[text()='Report']")
       const elements = await page.$x("//span[text()='Report']")
-      await elements[0].click() 
+      await elements[0].click()
 
-      var randomCharsNoSpaces='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-      var randomCharsSpaces=' 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-      
+      var randomCharsNoSpaces = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      var randomCharsSpaces = ' 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
       //enter name
       await page.focus('input[data-original_id="name"]')
       await page.keyboard.type(generateRandomString(16, randomCharsNoSpaces))
@@ -54,18 +53,20 @@ async function main() {
       await page.keyboard.type(generateRandomString(69, randomCharsSpaces))
 
       //click submit
-      await page.click('button[type="submit"]') 
+      await page.click('button[type="submit"]')
 
       //wait for success 
       await page.waitForXPath("//p[text()='Your report has been sent, thank you!']")
 
       numberOfSubmissions++
       console.log(`submitted ${numberOfSubmissions} forms`)
-    }catch(err){
-      console.log('program error:', err)
     }
-
+  } catch (err) {
+    console.log('program error:', err)
+    await delay(7000)
   }
+
+
 
 };
 
